475aceb5df0f68041dc81bebf01f0756355ff2fa
Veronika <sashamasyana@yandex.ru>
2020-12-16 19:39:43 +0300

    added ShapeAccumulator.kt

--- src/figures/Main.kt
+++ src/figures/Main.kt
@@ -26,7 +26,7 @@ fun main() {
     val rectangle2 = Rectangle(3.0, 14.0,).also(::println);
 
     //list of figures
-    ArrayList<Shape>().apply {
+    val shapes = ArrayList<Shape>().apply {
         add(rectangle)
         add(rectangle2)
         add(triangle)
@@ -69,7 +69,7 @@ fun main() {
                 largestAreaFigure = figure
             }
             //get figure with the smallest area
-            if (figure.getCalcPerimeter()<smallestAreaFigure.getCalcPerimeter()) {
+            if (figure.getCalcArea()<smallestAreaFigure.getCalcArea()) {
                 smallestAreaFigure = figure
             }
         }
@@ -86,4 +86,97 @@ fun main() {
                 "in $smallestAreaFigure.\n"
         )
     }
+
+
+
+    //_______________________________________________________________________//
+    //tests for ShapeAccumulator
+
+    println("\n\n\n----------tests for ShapeAccumulator\n")
+    val shapeAccumulatorTriangle: ShapeAccumulator<Triangle> = ShapeAccumulator(triangle)
+    val maxAreaShape = shapeAccumulatorTriangle.getMaxAreaShape()
+    val maxPerimeterShape = shapeAccumulatorTriangle.getMaxPerimeterShape()
+    val minAreaShape = shapeAccumulatorTriangle.getMinAreaShape()
+    val minPerimeterShape = shapeAccumulatorTriangle.getMinPerimeterShape()
+    val totalArea = shapeAccumulatorTriangle.getTotalArea()
+    val totalPerimeter = shapeAccumulatorTriangle.getTotalPerimeter()
+
+    //print all the parameters to the console
+    println("\n Total perimeter = $totalPerimeter, " +
+            "total area = $totalArea.\n" +
+            "\nLargest perimeter = ${maxPerimeterShape.getCalcPerimeter()} " +
+            "in $maxPerimeterShape, " +
+            "\nlargest area = ${maxAreaShape.getCalcArea()} " +
+            "in $maxAreaShape.\n" +
+            "\nSmallest perimeter = ${minPerimeterShape.getCalcPerimeter()} " +
+            "in $minPerimeterShape, " +
+            "\nsmallest area = ${minAreaShape.getCalcArea()} " +
+            "in $minAreaShape.\n"
+    )
+
+
+    shapeAccumulatorTriangle.addAll(arrayListOf(triangle, triangle2))
+    val maxAreaShape2 = shapeAccumulatorTriangle.getMaxAreaShape()
+    val maxPerimeterShape2 = shapeAccumulatorTriangle.getMaxPerimeterShape()
+    val minAreaShape2 = shapeAccumulatorTriangle.getMinAreaShape()
+    val minPerimeterShape2 = shapeAccumulatorTriangle.getMinPerimeterShape()
+    val totalArea2 = shapeAccumulatorTriangle.getTotalArea()
+    val totalPerimeter2 = shapeAccumulatorTriangle.getTotalPerimeter()
+
+    //print all the parameters to the console
+    println("\n Total perimeter = $totalPerimeter2, " +
+            "total area = $totalArea2.\n" +
+            "\nLargest perimeter = ${maxPerimeterShape2.getCalcPerimeter()} " +
+            "in $maxPerimeterShape2, " +
+            "\nlargest area = ${maxAreaShape2.getCalcArea()} " +
+            "in $maxAreaShape2.\n" +
+            "\nSmallest perimeter = ${minPerimeterShape2.getCalcPerimeter()} " +
+            "in $minPerimeterShape2, " +
+            "\nsmallest area = ${minAreaShape2.getCalcArea()} " +
+            "in $minAreaShape2.\n"
+    )
+
+    shapeAccumulatorTriangle.add(triangle)
+    val maxAreaShape21 = shapeAccumulatorTriangle.getMaxAreaShape()
+    val maxPerimeterShape21 = shapeAccumulatorTriangle.getMaxPerimeterShape()
+    val minAreaShape21 = shapeAccumulatorTriangle.getMinAreaShape()
+    val minPerimeterShape21 = shapeAccumulatorTriangle.getMinPerimeterShape()
+    val totalArea21 = shapeAccumulatorTriangle.getTotalArea()
+    val totalPerimeter21 = shapeAccumulatorTriangle.getTotalPerimeter()
+
+    //print all the parameters to the console
+    println("\n Total perimeter = $totalPerimeter21, " +
+            "total area = $totalArea21.\n" +
+            "\nLargest perimeter = ${maxPerimeterShape21.getCalcPerimeter()} " +
+            "in $maxPerimeterShape21, " +
+            "\nlargest area = ${maxAreaShape21.getCalcArea()} " +
+            "in $maxAreaShape21.\n" +
+            "\nSmallest perimeter = ${minPerimeterShape21.getCalcPerimeter()} " +
+            "in $minPerimeterShape21, " +
+            "\nsmallest area = ${minAreaShape21.getCalcArea()} " +
+            "in $minAreaShape21.\n"
+    )
+
+
+    val shapeAccumulator: ShapeAccumulator<Shape> = ShapeAccumulator(shapes)
+    val maxAreaShape3 = shapeAccumulator.getMaxAreaShape()
+    val maxPerimeterShape3 = shapeAccumulator.getMaxPerimeterShape()
+    val minAreaShape3 = shapeAccumulator.getMinAreaShape()
+    val minPerimeterShape3 = shapeAccumulator.getMinPerimeterShape()
+    val totalArea3 = shapeAccumulator.getTotalArea()
+    val totalPerimeter3 = shapeAccumulator.getTotalPerimeter()
+
+    //print all the parameters to the console
+    println("\n Total perimeter = $totalPerimeter3, " +
+            "total area = $totalArea3.\n" +
+            "\nLargest perimeter = ${maxPerimeterShape3.getCalcPerimeter()} " +
+            "in $maxPerimeterShape3, " +
+            "\nlargest area = ${maxAreaShape3.getCalcArea()} " +
+            "in $maxAreaShape3.\n" +
+            "\nSmallest perimeter = ${minPerimeterShape3.getCalcPerimeter()} " +
+            "in $minPerimeterShape3, " +
+            "\nsmallest area = ${minAreaShape3.getCalcArea()} " +
+            "in $minAreaShape3.\n"
+    )
+
 }
---
+++ src/figures/ShapeAccumulator.kt
@@ -0,0 +1,111 @@
+package figures
+
+class ShapeAccumulator<T: Shape>(shapes: Collection<T>) {
+    private val shapes = ArrayList<T>()
+
+    //constructors
+    init {
+        require(!shapes.isEmpty()) {"Collection is empty."}
+
+        this.shapes.addAll(shapes.toList())
+    }
+
+    constructor(shape: T) : this(arrayListOf(shape))
+
+    fun add(shape: T) {
+        this.shapes.add(shape)
+    }
+
+    fun addAll(shapes: Collection<T>) {
+        this.shapes.addAll(shapes)
+    }
+
+    fun getMaxAreaShape(): Shape {
+        var maxAreaShape = this.shapes[0]
+        for (figure in shapes) {
+            //get figure with the largest area
+            if (figure.getCalcArea()>maxAreaShape.getCalcArea()) {
+                maxAreaShape = figure
+            }
+        }
+
+        return maxAreaShape
+    }
+
+    fun getMinAreaShape(): Shape {
+        var minAreaShape = this.shapes[0]
+        for (figure in shapes) {
+            //get figure with the smallest area
+            if (figure.getCalcArea()<minAreaShape.getCalcArea()) {
+                minAreaShape = figure
+            }
+        }
+
+        return minAreaShape
+    }
+
+    fun getMaxPerimeterShape(): Shape {
+        var maxPerimeterShape = this.shapes[0]
+        for (figure in shapes) {
+            //get figure with the largest perimeter
+            if (figure.getCalcPerimeter()>maxPerimeterShape.getCalcPerimeter()) {
+                maxPerimeterShape = figure
+            }
+        }
+
+        return maxPerimeterShape
+    }
+
+    fun getMinPerimeterShape(): Shape {
+        var minPerimeterShape = this.shapes[0]
+        for (figure in shapes) {
+            //get figure with the smallest perimeter
+            if (figure.getCalcPerimeter()<minPerimeterShape.getCalcPerimeter()) {
+                minPerimeterShape = figure
+            }
+        }
+
+        return minPerimeterShape
+    }
+
+    fun getTotalArea(): Double {
+        var totalArea = 0.0
+        for (figure in shapes) {
+            //get total area
+            totalArea += figure.getCalcArea()
+        }
+
+        return totalArea
+    }
+
+    fun getTotalPerimeter(): Double {
+        var totalPerimeter = 0.0
+        for (figure in shapes) {
+            //get total perimeter
+            totalPerimeter += figure.getCalcPerimeter()
+        }
+
+        return totalPerimeter
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ShapeAccumulator<*>
+
+        if (shapes != other.shapes) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return shapes.hashCode()
+    }
+
+    override fun toString(): String {
+        return "ShapeAccumulator(shapes=$shapes)"
+    }
+
+
+}
